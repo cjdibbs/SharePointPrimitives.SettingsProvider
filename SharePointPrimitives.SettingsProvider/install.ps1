818a8c2e146a92e89b4d92260c2c5f1394460445
Chris Dibbs <cjdibbs@gmail.com>
2011-01-05 12:39:34 -0500

    set up to build a wsp

---
+++ SharePointPrimitives.SettingsProvider/install.ps1
@@ -0,0 +1,9 @@
+﻿Wspbuilder.exe
+
+stsadm -o retractsolution -name SharePointPrimitives.SettingsProvider.wsp -local
+stsadm -o execadmsvcjobs
+stsadm -o deletesolution -name  SharePointPrimitives.SettingsProvider.wsp
+
+stsadm -o addsolution -filename SharePointPrimitives.SettingsProvider.wsp
+stsadm -o deploysolution -name sharepointprimitives.settingsprovider.wsp -local -allowgacdeployment
+stsadm -o execadmsvcjobs
