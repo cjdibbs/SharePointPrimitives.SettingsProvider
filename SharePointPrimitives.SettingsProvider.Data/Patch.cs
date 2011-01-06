4943322fa0e574bb2741699a9fb622dff8598ea0
Chris Dibbs <cjdibbs@gmail.com>
2011-01-06 15:45:37 -0500

    added inserts into the apply command

--- SharePointPrimitives.SettingsProvider.Data/Patch.cs
+++ SharePointPrimitives.SettingsProvider.Data/Patch.cs
@@ -8,11 +8,6 @@ using System.Xml.Linq;
 namespace SharePointPrimitives.SettingsProvider {
     public class Patch {
 
-        public struct ApplyOptions {
-            public bool UseExistingConnectionStrings { get; set; }
-            public bool MustKnowCurrentValueToDelete { get; set; }
-        }
-
         public class Action {
             public enum ActionType {
                 Update,
@@ -24,16 +19,18 @@ namespace SharePointPrimitives.SettingsProvider {
             public bool IsConnectionString { get; set; }
             public string Name { get; set; }
             public string Value { get; set; }
+            //public string ValueType { get; set; }
         }
 
         public string Assembly { get; set; }
         public string Section { get; set; }
         public List<Action> Actions { get; private set; }
-        
+
 
         public Patch() { Actions = new List<Action>(); }
 
-        public Patch(SnapShot inital) : this(){
+        public Patch(SnapShot inital)
+            : this() {
             this.Assembly = inital.Assembly;
             this.Section = inital.Section;
             foreach (var setting in inital.Settings)
@@ -54,7 +51,8 @@ namespace SharePointPrimitives.SettingsProvider {
         }
 
 
-        public Patch(SnapShot from, SnapShot to) : this(){
+        public Patch(SnapShot from, SnapShot to)
+            : this() {
             this.Section = to.Section;
             this.Assembly = to.Assembly;
 
@@ -62,7 +60,7 @@ namespace SharePointPrimitives.SettingsProvider {
             DiffDictionary(from.ConnectionStrings, to.ConnectionStrings, true);
         }
 
-        private void DiffDictionary(Dictionary<string, string> from, Dictionary<string,string> to, bool areConnectionStrings) {
+        private void DiffDictionary(Dictionary<string, string> from, Dictionary<string, string> to, bool areConnectionStrings) {
             foreach (var setting in to) {
                 if (!from.ContainsKey(setting.Key))
                     Actions.Add(new Action() {
@@ -97,9 +95,9 @@ namespace SharePointPrimitives.SettingsProvider {
         public XElement ToXml() {
             XElement ret = new XElement("patch",
                 new XAttribute("assembly", Assembly),
-                new XAttribute("section",Section)
+                new XAttribute("section", Section)
             );
-            foreach(Action action in Actions)
+            foreach (Action action in Actions)
                 ret.Add(
                     new XElement("action",
                         new XAttribute("type", action.Type.ToString()),
@@ -111,13 +109,58 @@ namespace SharePointPrimitives.SettingsProvider {
             return ret;
         }
 
+        public override string ToString() {
+            return String.Join("\n",
+                Actions.Select(a => String.Format("{0} {1}='{2}'", a.Type, a.Name, a.Value))
+                .ToArray()
+            );
+        }
+
         //public static Patch FromXml(XElement element) {
         //}
 
-        //public void Apply(ApplyOptions options) {
-        //}
+        public struct ApplyOptions {
+            public bool UseExistingConnectionStrings { get; set; }
+            public bool MustKnowCurrentValueToDelete { get; set; }
+            public bool CleanUpZombiedConnectionStrings { get; set; }
+        }
 
-        //public XElement Test() {
-        //}
+        public void Apply(ApplyOptions options) {
+            using (var database = new SettingsProviderDatabase()) {
+                var section = database.Sections.FirstOrDefault(s => s.Name == Section);
+                if (section == null) {
+                    section = new Section() { Name = Section, AssemblyName = Assembly };
+                    database.AddToSections(section);
+                }
+
+                foreach (Action action in Actions) {
+                    switch (action.Type) {
+                        case Action.ActionType.Insert:
+                            if (action.IsConnectionString) {
+                                var name = new SqlConnectionName() {
+                                    Name = action.Name
+                                };
+                                var connection = new SqlConnectionString() {
+                                    ConnectionString = action.Value
+                                };
+
+                                database.AddToSqlConnectionNames(name);
+                                database.AddToSqlConnectionStrings(connection);
+
+                                name.Section = section;
+                                name.SqlConnectionString = connection;
+                            } else {
+                                var setting = new ApplicationSetting() {
+                                    Name = action.Name,
+                                    Value = action.Value
+                                };
+                                setting.Section = section;
+                            }
+                            break;
+                    }
+                }
+                database.SaveChanges();
+            }
+        }
     }
 }
